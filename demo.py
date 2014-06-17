b821060662fac71db7a115e9b7a463d6c783f68e
sujeetpatel <sujeet@openerp4you.in>
2014-06-17 14:42:44 +0530

    I m ready to add

---
+++ demo.py
@@ -0,0 +1,9 @@
+x = 10 
+if x > 5:
+	print "my first file", x
+print "my First File"
+
+https://github.com/sujeetpatel/openerp4you.git
+https://github.com/sujeetpatel/openerp4you.git
+
+https://github.com/blog/1178-collaborating-on-github-with-subversion
